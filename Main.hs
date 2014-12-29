10a4054ef8d876f944f2a9036d53b4340474cdc1
Michael Tolly <miketolly@gmail.com>
2014-12-28 22:27:34 -0600

    throw out .tar.gz, just embed the files uncompressed
    had some issues (extraction sometimes failed).
    this is way simpler and zipped exe size is unchanged

--- Main.hs
+++ Main.hs
@@ -1,23 +1,21 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Main where
 
-import           Codec.Archive.Tar      (extract)
-import           Codec.Compression.GZip (decompress)
-import qualified Data.ByteString.Lazy   as BL
-import           Data.Char              (toLower)
-import           Data.FileEmbed         (embedFile)
-import           Data.List              (partition)
-import           System.Directory       (getCurrentDirectory)
-import qualified System.Directory       as Dir
-import           System.Environment     (getArgs)
-import           System.FilePath        (takeExtension, (</>))
-import           System.Info            (os)
-import           System.IO.Temp         (withSystemTempDirectory)
-import           System.Process         (callProcess)
+import           Control.Monad      (forM_)
+import qualified Data.ByteString    as B
+import           Data.Char          (toLower)
+import           Data.FileEmbed     (embedDir)
+import           Data.List          (partition)
+import           System.Directory   (getCurrentDirectory)
+import qualified System.Directory   as Dir
+import           System.Environment (getArgs)
+import           System.FilePath    (takeExtension, (</>))
+import           System.Info        (os)
+import           System.IO.Temp     (withSystemTempDirectory)
+import           System.Process     (callProcess)
 
-tarGz :: BL.ByteString
-tarGz = BL.fromStrict $(embedFile "data.tar.gz")
+dataFiles :: [(FilePath, B.ByteString)]
+dataFiles = $(embedDir "data/")
 
 main :: IO ()
 main = do
@@ -31,12 +29,8 @@ main = do
   withSystemTempDirectory "magmyx" $ \tmp -> do
 
     Dir.setCurrentDirectory tmp
-    let tar = tmp </> "data.tar"
-    BL.writeFile tar $ decompress tarGz
-    extract tmp tar
-    let extracted = tmp </> "data"
-
-    Dir.setCurrentDirectory extracted
+    Dir.createDirectory "gen"
+    forM_ dataFiles $ uncurry B.writeFile
     let exe = if null c3 then "MagmaCompiler.exe" else "MagmaCompilerC3.exe"
     if os == "mingw32"
       then callProcess exe argv''
--- zip.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-tar -cvzf data.tar.gz data/
